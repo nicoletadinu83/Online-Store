2b45d8304d8f3318c6bfe1339109bf2791c57f65
AlexandruNedieanu <nedieanualex@yahoo.com>
2020-09-19 15:22:51 +0300

    Class Author update

--- onlinestore/src/main/java/com/sda/onlinestore/model/AuthorModel.java
+++ onlinestore/src/main/java/com/sda/onlinestore/model/AuthorModel.java
@@ -1,18 +1,29 @@
 package com.sda.onlinestore.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity(name = "Author")
 @Table
 public class AuthorModel {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "author_id")
+    private int id;
 
     @Column(name = "firstName")
     private String firstName;
 
     @Column(name = "lastName")
     private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;

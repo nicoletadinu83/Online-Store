8ae49877580595d3a95ebd97da63de9ac5ea766b
AlexLungoci <alexandru.lungoci@yahoo.com>
2020-09-19 13:02:27 +0300

    CartModel

--- onlinestore/src/main/java/com/sda/onlinestore/model/CartModel.java
+++ onlinestore/src/main/java/com/sda/onlinestore/model/CartModel.java
@@ -1,4 +1,13 @@
 package com.sda.onlinestore.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartModel {
+
+    private List<Product> productList = new ArrayList<>();
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }

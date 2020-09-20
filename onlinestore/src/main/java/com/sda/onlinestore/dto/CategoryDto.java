9ca34fed4e81ea3a0f5183d5475d8dc0e8767b00
Draghici Mihai <mihaidc28@gmail.com>
2020-09-20 12:08:09 +0300

    Repository

--- onlinestore/src/main/java/com/sda/onlinestore/dto/CategoryDto.java
+++ onlinestore/src/main/java/com/sda/onlinestore/dto/CategoryDto.java
@@ -1,4 +1,47 @@
 package com.sda.onlinestore.dto;
 
+import com.sda.onlinestore.model.CategoryModel;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDto {
+
+    private long id;
+    private String name;
+    private List<CategoryModel> subcategory= new ArrayList<>();
+    private CategoryModel parentCategory;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CategoryModel> getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(List<CategoryModel> subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public CategoryModel getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(CategoryModel parentCategory) {
+        this.parentCategory = parentCategory;
+    }
 }
--- onlinestore/src/main/java/com/sda/onlinestore/repository/CategoryRepository.java
+++ onlinestore/src/main/java/com/sda/onlinestore/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.sda.onlinestore.repository;
 
-public interface CategoryRepository {
+import com.sda.onlinestore.model.CategoryModel;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryModel, Long>{
 }

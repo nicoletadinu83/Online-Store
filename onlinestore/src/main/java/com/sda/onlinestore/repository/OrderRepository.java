1e8a74a33d811279b8313fd6848e03246868c651
nicoletadinu83 <nicoletadinu1009@gmail.com>
2020-09-20 12:08:54 +0300

    no message

--- onlinestore/src/main/java/com/sda/onlinestore/repository/OrderRepository.java
+++ onlinestore/src/main/java/com/sda/onlinestore/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.sda.onlinestore.repository;
 
-public interface OrderRepository {
+import com.sda.onlinestore.model.OrderModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository <OrderModel, Long> {
 }
--- onlinestore/src/main/java/com/sda/onlinestore/repository/ProductRepository.java
+++ onlinestore/src/main/java/com/sda/onlinestore/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.sda.onlinestore.repository;
 
-public class ProductRepository {
+import com.sda.onlinestore.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, Long> {
 }
+

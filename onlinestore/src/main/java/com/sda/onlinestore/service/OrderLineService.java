410b5d814eb059fb30133dcc1607f40b030b5495
AlexLungoci <alexandru.lungoci@yahoo.com>
2020-09-28 23:09:00 +0300

    OrderLineService &Rep

---
+++ onlinestore/src/main/java/com/sda/onlinestore/service/OrderLineService.java
@@ -0,0 +1,17 @@
+package com.sda.onlinestore.service;
+
+import com.sda.onlinestore.dto.OrderLineDto;
+
+import java.util.List;
+
+public interface OrderLineService {
+
+    List<OrderLineDto> getOrderLines();
+
+    void addOrderLine(OrderLineDto orderLineDto);
+
+    void deleteOrderLine(Long id);
+
+    void updateOrderLineDto(OrderLineDto orderLineDto);
+
+}
